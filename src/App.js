47d45957d03551d0da942d0002ffcfbc84376b1c
ER Faisal Maulabhi <xlover707@gmail.com>
2021-02-25 12:24:23 +0600

    React Very Inportant Concept

--- src/App.js
+++ src/App.js
@@ -2,15 +2,75 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks= ['Razzak', 'Alomgir', 'Shovo','Salman Sha', 'Bappa Raj']
+
+  const products = [
+    {name: 'Photoshop', price: '$90.99'},
+    {name: 'Illustrator', price: '$60.99'},
+    {name: 'PDF Reader', price: '$6.99'}
+  ]
+
+  const nayokNames = nayoks.map(nayok => nayok)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit Done <code>src/App.js</code> and save to reload.
+          I am a React Student!
         </p>
-  
+
+        <ul>
+
+          {
+            nayoks.map(nayok => <li> {nayok} </li>)
+          }
+          <br/>
+          {
+            products.map (product => <li> {product.name} </li> )
+          }
+        </ul>
+
+        {
+          products.map (pd => <Product product={pd}></Product>)
+        }
+<br/>
+        {/* 1st defined style */}
+        {/* <Product name={products[0].name} price={products[0].price}></Product> */}
+        <Product product={products[0]}></Product>
+        <Product></Product>
+        <Product></Product>
+
+        <Person name='Rana Hasan' job='Mariner'></Person>
+        <Person name='Nayeem' job='Garments'></Person>
       </header>
+    </div>
+  );
+}
+
+function Product (props){
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '10px',
+    backgroundColor: 'lightgrey',
+    height: '200px',
+    width: '200px',
+    float: 'left'
+  }
+  // const {name, price} = props.product;
+  return (
+    <div style = {productStyle}>
+      <h4>{props.product?.name}</h4>
+      <h3>{props.product?.price}</h3>
+      <button>By now</button>
+    </div>
+  );
+}
+
+function Person (props){
+  return (
+    <div style={{border: '2px solid red', width: '400px', margin: '20px'}}>
+      <h3>My Name: {props.name}</h3>
+      <p>My Profession: {props.job} </p>
     </div>
   );
 }
